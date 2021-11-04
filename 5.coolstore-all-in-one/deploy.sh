af692517ff6f7ef05c99f610415c2f0425d85801
fuangwith@gmail.com <fuangwith@gmail.com>
2021-11-04 11:27:36 +0700

    update annotation

--- 5.coolstore-all-in-one/deploy.sh
+++ 5.coolstore-all-in-one/deploy.sh
@@ -13,7 +13,7 @@ oc label dc/inventory-database app.kubernetes.io/part-of=inventory --overwrite  
 oc label dc/inventory app.openshift.io/runtime=quarkus --overwrite  -n coolstore-shop
 oc label dc/inventory app.kubernetes.io/part-of=inventory --overwrite  -n coolstore-shop
 
-oc annotate dc/inventory app.openshift.io/connects-to=inventory-database --overwrite  -n coolstore-shop
+oc annotate dc/inventory app.openshift.io/connects-to=inventory-database  -n coolstore-shop
 
 
 ### Catalog ###
@@ -25,7 +25,7 @@ oc label dc/catalog-database app.openshift.io/runtime=postgresql --overwrite -n 
 oc label dc/catalog-springboot app.openshift.io/runtime=spring-boot --overwrite -n coolstore-shop 
 oc label dc/catalog-database app.kubernetes.io/part-of=catalog --overwrite -n coolstore-shop 
 oc label dc/catalog-springboot app.kubernetes.io/part-of=catalog --overwrite -n coolstore-shop 
-oc annotate dc/catalog-springboot app.openshift.io/connects-to=catalog-database --overwrite -n coolstore-shop 
+oc annotate dc/catalog-springboot app.openshift.io/connects-to=catalog-database -n coolstore-shop 
 
 
 ### Coolstore ###
@@ -38,7 +38,7 @@ oc label dc/coolstore-postgresql app.openshift.io/runtime=postgresql --overwrite
 oc label dc/coolstore app.openshift.io/runtime=jboss --overwrite -n coolstore-shop 
 oc label dc/coolstore-postgresql app.kubernetes.io/part-of=coolstore --overwrite -n coolstore-shop 
 oc label dc/coolstore app.kubernetes.io/part-of=coolstore --overwrite -n coolstore-shop 
-oc annotate dc/coolstore app.openshift.io/connects-to=coolstore-postgresql --overwrite -n coolstore-shop 
+oc annotate dc/coolstore app.openshift.io/connects-to=coolstore-postgresql -n coolstore-shop 
 
 
 CATALOG_BASE_URL="http://$(oc get route.route.openshift.io/catalog-springboot -n coolstore-shop -o 'jsonpath={.spec.host}')//services/products"
@@ -46,5 +46,5 @@ oc set env dc/coolstore --env=CATALOG_BASE_URL=$CATALOG_BASE_URL --overwrite=tru
 
 
 ### 
-oc annotate dc/coolstore app.openshift.io/connects-to=catalog-springboot --overwrite -n coolstore-shop 
-oc annotate dc/catalog-springboot app.openshift.io/connects-to=inventory --overwrite -n coolstore-shop 
+oc annotate dc/coolstore app.openshift.io/connects-to=catalog-springboot,coolstore-postgresql --overwrite -n coolstore-shop 
+oc annotate dc/catalog-springboot app.openshift.io/connects-to=inventory,catalog-database --overwrite -n coolstore-shop 
