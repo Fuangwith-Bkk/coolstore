f6224e6fb174b1765c3c659dead7be3600728731
fuangwith@gmail.com <fuangwith@gmail.com>
2021-11-04 14:32:44 +0700

    check-in source code

---
+++ code/README.md
@@ -0,0 +1,9 @@
+The CCN Roadshow(Dev Track) Module 2 Labs 2021
+===
+These labs provide templates, generated Java codes, empty configuration for each lab that the workshop attenddees will develop during the roadshow.
+The included Java projects and/or installation files are here: 
+
+* Catalog - Spring Boot project
+* Inventory - Quarkus project
+* Monolith - JBoss EAP project
+* Monitoring - Multiple monitoring tools such as Prometheus, Grafana, Jaeger
---
+++ code/monolith/scripts/deploy-coolstore.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+USERXX=$1
+
+if [ -z $USERXX ]
+  then
+    echo "Usage: Input your username like deploy-boolstore.sh user1"
+    exit;
+fi
+
+echo Your username is $USERXX
+
+echo Deploy coolstore project........
+
+oc project $USERXX-coolstore-dev || oc new-project $USERXX-coolstore-dev
+oc delete dc,deployment,bc,build,svc,route,pod,is --all
+
+echo "Waiting 30 seconds to finialize deletion of resources..."
+sleep 30
+
+oc new-app coolstore-monolith-binary-build  --as-deployment-config -p USER_ID=$USERXX
+
+mvn clean package -Popenshift -f $CHE_PROJECTS_ROOT/cloud-native-workshop-v2m2-labs/monolith/
+oc start-build coolstore --from-file $CHE_PROJECTS_ROOT/cloud-native-workshop-v2m2-labs/monolith/deployments/ROOT.war
+
+oc label dc/coolstore-postgresql app.openshift.io/runtime=postgresql --overwrite && \
+oc label dc/coolstore app.openshift.io/runtime=jboss --overwrite && \
+oc label dc/coolstore-postgresql app.kubernetes.io/part-of=coolstore --overwrite && \
+oc label dc/coolstore app.kubernetes.io/part-of=coolstore --overwrite && \
+oc annotate dc/coolstore app.openshift.io/connects-to=coolstore-postgresql --overwrite && \
+oc annotate dc/coolstore app.openshift.io/vcs-uri=https://github.com/RedHat-Middleware-Workshops/cloud-native-workshop-v2m2-labs.git --overwrite && \
+oc annotate dc/coolstore app.openshift.io/vcs-ref=ocp-4.7 --overwrite
