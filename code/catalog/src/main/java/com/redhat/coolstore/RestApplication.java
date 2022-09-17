0012cc85efab39dd59069bfa7247c3ce9cf18184
Fuangwith-Bkk <fuangwith@gmail.com>
2022-09-17 13:21:47 +0700

    cors

--- code/catalog/src/main/java/com/redhat/coolstore/RestApplication.java
+++ code/catalog/src/main/java/com/redhat/coolstore/RestApplication.java
@@ -3,6 +3,8 @@ package com.redhat.coolstore;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 @SpringBootApplication
 @EnableFeignClients
@@ -11,4 +13,18 @@ public class RestApplication {
     public static void main(String[] args) {
         SpringApplication.run(RestApplication.class, args);
     }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                String urls = env.getProperty("cors.urls");
+                CorsRegistration reg = registry.addMapping("/**");
+                for(String url: urls.split(",")) {
+                    reg.allowedOrigins(url);
+                }
+            }
+        };
+    }    
 }
